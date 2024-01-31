dfca88882ec5365ad33eaff34a454e158968694a
prashant singh <havtohell1857@gmail.com>
2024-01-31 16:59:01 +0530

    Basic Setup Done Dependency Injection Added and other libraries created

---
+++ SentinelOne.Authentication.DataAccess/DataSources/MockDatabase.cs
@@ -0,0 +1,12 @@
+﻿using SentinelOne.Authentication.DataAccess.Model;
+
+namespace SentinelOne.Authentication.DataAccess.DataSources;
+
+public static class MockDatabase
+{
+    public static List<User> Users { get; } = new List<User>
+    {
+        new User { Username = "user1", PasswordHash = "hashedPassword1" },
+        new User { Username = "user2", PasswordHash = "hashedPassword2" },
+    };
+}
