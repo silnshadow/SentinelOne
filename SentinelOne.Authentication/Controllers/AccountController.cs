c827a84ebc56314a0dc7a3053348e34fdd1381a4
prashant singh <havtohell1857@gmail.com>
2024-02-06 12:16:54 +0530

    Google authentication implmentation is in progress

---
+++ SentinelOne.Authentication/Controllers/AccountController.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SentinelOne.Authentication.Controllers
+{
+    public class AccountController : ControllerBase
+    {
+        
+    }
+}
---
+++ SentinelOne.Authentication/Models/LoginViewModel.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using System.ComponentModel.DataAnnotations;
+namespace SentinelOne.Authentication.Models;
+
+public class LoginViewModel
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+
+    [Required]
+    [DataType(DataType.Password)]
+    public string Password { get; set; }
+
+    [Display(Name = "Remember Me")]
+    public bool RememberMe { get; set; }
+
+    public string? ReturnUrl { get; set; }
+
+    // AuthenticationScheme is in Microsoft.AspNetCore.Authentication namespace
+    public IList<AuthenticationScheme>? ExternalLogins { get; set; }
+}
